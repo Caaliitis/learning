386dbc8fb4ee6aec56b13752e64027c0ed330424
Daniels Zaltāns <daniels.zaltans@gmail.com>
2022-01-04 21:17:31 +0200

    trshais meginajums

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-//laikam kkas sanaca ar commitu !!!!
+//laikam kkas sanaca ar commitu !!!! 23
