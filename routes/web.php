d2b6b1e2a4969023b715287e29742f7c9305990f
Daniels Zaltāns <daniels.zaltans@gmail.com>
2022-01-05 00:44:47 +0200

    firewall uzlikts

--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,27 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-Route::get('/', function () {
+// If route doesnt exist, then redrect to /
+Route::fallback(function () {
+    return redirect('/');
+});    
+
+//home page a.k.a. welcome page
+Route::get('/', function(){
     return view('welcome');
 });
 
+//this enables auth routes like /login. Also enables auth header menu Login/register
 Auth::routes();
 
-Route::get('post', [App\Http\Controllers\PostController::class, 'index'])->name('post.list');
-Route::get('post/create', [App\Http\Controllers\PostController::class, 'create'])->name('post.create.view');
-Route::post('post', [App\Http\Controllers\PostController::class, 'store'])->name('post.create.form');
-Route::get('post/{post}/edit', [App\Http\Controllers\PostController::class, 'edit'])->name('post.edit.view');
-Route::post('post/{post}/edit', [App\Http\Controllers\PostController::class, 'update'])->name('post.edit.form');
-Route::get('post/{post}', [App\Http\Controllers\PostController::class, 'show'])->name('post.get.view');
-Route::post('post/{post}/delete', [App\Http\Controllers\PostController::class, 'destroy'])->name('post.delete');
+Route::group(['middleware' => 'auth'], function(){
+
+    Route::get('post', [App\Http\Controllers\PostController::class, 'index'])->name('post.list');
+    Route::post('post', [App\Http\Controllers\PostController::class, 'store'])->name('post.create.form');
+    Route::get('post/create', [App\Http\Controllers\PostController::class, 'create'])->name('post.create.view');
+    Route::get('post/{post}/edit', [App\Http\Controllers\PostController::class, 'edit'])->name('post.edit.view');
+    Route::post('post/{post}/edit', [App\Http\Controllers\PostController::class, 'update'])->name('post.edit.form');
+    Route::get('post/{post}', [App\Http\Controllers\PostController::class, 'show'])->name('post.get.view');
+    Route::post('post/{post}/delete', [App\Http\Controllers\PostController::class, 'destroy'])->name('post.delete');
+
+});
