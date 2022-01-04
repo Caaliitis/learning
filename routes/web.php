78dd164eca7fb58350a4a4fca5d9ea909e3378a3
Daniels Zaltāns <daniels.zaltans@gmail.com>
2022-01-04 19:11:20 +0200

    plsss work

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,8 +20,10 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\PostController::class, 'index'])->name('home');
+Route::get('post/create', [App\Http\Controllers\PostController::class, 'create']);
+Route::post('post', [App\Http\Controllers\PostController::class, 'store']);
+Route::get('post/{post}/edit', [App\Http\Controllers\PostController::class, 'edit']);
+Route::get('post/{post}', [App\Http\Controllers\PostController::class, 'show']);
+Route::put('post/{post}', [App\Http\Controllers\PostController::class, 'update']);
+Route::delete('post/{post}', [App\Http\Controllers\PostController::class, 'destroy']);
